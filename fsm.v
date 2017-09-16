7bcf60be5b9adbce71dd747f231a493acc261db0
Larry Liu <yunpengliu199703@gmail.com>
2017-09-17 06:38:54 +0800

    fsm parameter updated

--- fsm.v
+++ fsm.v
@@ -2,9 +2,10 @@
 module fsm(clk, in, out);
 
 	// inputs and outputs - NOTE: will rename in and out later
+	parameter width = 8;
 	input clk;
-	input [1:0] in;
-	output reg [1:0] out;
+	input [width-1:0] in;
+	output reg [width-1:0] out;
 	
 	always_ff @(posedge clk)
 		out <= in;
