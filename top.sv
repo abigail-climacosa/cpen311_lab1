adbd7818a7779b727c03af40c452e545ab306112
Abigail Climacosa <Abigail Climacosa>
2017-09-22 02:32:06 -0700

    Corrected the "So" frequency from 738 to 783 Hz

--- top.sv
+++ top.sv
@@ -3,7 +3,7 @@
 `define Re 3'b001         // 587 Hz
 `define Mi 3'b010         // 659 Hz
 `define Fa 3'b011         // 698 Hz
-`define So 3'b100         // 738 Hz
+`define So 3'b100         // 783 Hz
 `define La 3'b101         // 880 Hz
 `define Ti 3'b110         // 987 Hz
 `define Do2 3'b111        // 1046 Hz
@@ -23,7 +23,7 @@ module top(clk, reset, switch_val, clk_out);
 			`Re: count_val =  32'hA65D;        // 42589
 			`Mi: count_val =  32'h9430;        // 37936
 			`Fa: count_val =  32'h8BE9;        // 35817
-			`So: count_val =   32'h8453;        // 33875
+			`So: count_val =   32'h7CB8;        // 33875
 			`La: count_val =  32'h6EF9;        // 28409
 			`Ti: count_val =           32'h62F1;        // 25329
 			`Do2: count_val =          32'h5D5D;       // 23901
