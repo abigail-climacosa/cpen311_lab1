07bd57e940602c3051a2e4a6d52904b3a95de341
Larry Liu <yunpengliu199703@gmail.com>
2017-09-23 01:21:15 +0800

    led update

--- led.v
+++ led.v
@@ -1,18 +1,16 @@
 //passing in the slower clk at top level eg. Basic_Organ_Solution
 module led_con(input logic clk,
-			   input logic reset
+			   input logic reset,
 			   output logic [9:0] leds);
 
-`define MAX_LED  3'h200 // 
-logic [9:0] leds;
+`define MAX_LED  10'b1000000000 // 
+//logic [9:0] led_re;
 
-// asynchronous reset for leds
-assign leds_re = reset ? 10'b1 : leds;
 
 always_ff @(posedge clk)
-	if(leds_re > `MAX_LED)
+	if(leds >= `MAX_LED ||reset)
 		leds <= 10'b1;
 	else
-		leds <= leds_re << 1;
+		leds <= (leds << 1) ;
 endmodule
 	
